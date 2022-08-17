05019efc01b44f321c666d947dc63f1284632d8a
Taesoo <tarrak123@gmail.com>
2022-08-17 14:49:21 -0400

    feat: Logo added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,16 @@ import { MdInfoOutline } from "react-icons/md";
 import { FaRegUserCircle } from "react-icons/fa";
 import { AnimatePresence } from "framer-motion";
 import { InfoModal, UserModal } from "./modals";
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
 interface Props {}
 
 export const Header: React.FC<Props> = () => {
   const [infoModalOpen, setInfoModalOpen] = useState(false);
   const [userModalOpen, setUserModalOpen] = useState(false);
+
+  const { theme } = useTheme();
 
   return (
     <div className="flex items-center justify-between w-full h-[50px] border-b-[.5px] border-slate-400 px-1 sm:px-8">
@@ -23,7 +27,11 @@ export const Header: React.FC<Props> = () => {
         {userModalOpen && <UserModal setIsOpen={setUserModalOpen} />}
       </AnimatePresence>
 
-      <h1 className="flex tracking-widest">WORDLE</h1>
+      <Image
+        src={theme === "dark" ? "/logo_dark.svg" : "/logo_light.svg"}
+        width={90}
+        height={50}
+      />
 
       <button
         className="flex items-center justify-center w-16"
